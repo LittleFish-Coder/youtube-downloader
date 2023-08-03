1aabef9215593a97f5b9e670a883786bc558a338
LittleFish-Coder <adrianyu890317@gmail.com>
2023-08-04 02:25:48 +0800

    upadte requirements.txt and enhance bash script

--- youtube_downloader.sh
+++ youtube_downloader.sh
@@ -1,3 +1,4 @@
+# !/bin/bash
 # this script needs to be run in the terminal and yt-dlp needs to be installed first
 
 # get the youtube video name
@@ -19,4 +20,14 @@
 # yt-dlp -f "bestvideo[ext=mp4][height<=1080]+bestaudio[ext=m4a]" --merge-output-format mp4 -o "%(title)s.%(ext)s" --force-overwrites https://youtu.be/PHwayuLBKLw
 
 # make the youtube url as a variable, which will later be given by the user
-yt-dlp -f "bestvideo[ext=mp4][height<=1080]+bestaudio[ext=m4a]" -o "%(title)s.%(ext)s" --force-overwrites $1
+# yt-dlp -f "bestvideo[ext=mp4][height<=1080]+bestaudio[ext=m4a]" -o "%(title)s.%(ext)s" --force-overwrites $1
+# you may use the command like this: bash ./youtube_downloader.sh https://youtu.be/PHwayuLBKLw
+
+# make it personalized as an app
+# you may use the command like this: bash ./youtube_downloader.sh
+
+# prompt the user to input the youtube url
+echo "Please input the youtube url: "
+read youtube_url
+# then download the video
+yt-dlp -f "bestvideo[ext=mp4][height<=1080]+bestaudio[ext=m4a]" -o "%(title)s.%(ext)s" --force-overwrites $youtube_url
