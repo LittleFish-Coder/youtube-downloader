e61f66dd56543c0e42faba87a67f5209ab4ba07b
LittleFish-Coder <adrianyu893017@gmail.com>
2025-02-04 23:21:00 +0800

    update app

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from moviepy.editor import *
-from pytube import YouTube
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
 import os
 from collections import defaultdict
+import re
 
 
 # output folder
@@ -12,71 +14,71 @@ output_folder = "output"
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
+def reformat_url(url):
+    """Reformat the YouTube video URL."""
+    youtu_be_pattern = re.compile(r"^https?://youtu\.be/")
+    youtube_pattern = re.compile(r"^https?://(www\.)?youtube\.com/watch\?v=([^&]+)")
 
-def progress_callback(stream, chunk, bytes_remaining):
-    size = stream.filesize
-    progress = (size - bytes_remaining) / size * 100
-    print(f"{progress:.2f}% downloaded")
+    if youtu_be_pattern.match(url):
+        return url
+    elif youtube_pattern.match(url):
+        video_id = youtube_pattern.match(url).group(2)
+        return f"https://youtu.be/{video_id}"
+    else:
+        return url
 
-
-resolutions = ["2160p", "1440p", "1080p", "720p", "480p"]
 
 st.title("YouTube Downloader")
-mp4_url = st.text_input("Enter MP4 URL:")
-if "youtube.com" in mp4_url or "youtu.be" in mp4_url:
+url = st.text_input("Enter Youtube URL:")
+if "youtube.com" in url or "youtu.be" in url:
     try:
+        url = reformat_url(url)
+        
         # Create a YouTube object using the video URL
-        yt = YouTube(mp4_url, on_progress_callback=progress_callback, use_oauth=True)
+        yt = YouTube(url, on_progress_callback=on_progress, use_oauth=True)
 
         # Display the video details to the user
-        st.markdown(f"## {yt.title} - {yt.author}")
+        st.markdown(f"## {yt.title}")
 
         # show the picture of the video
         st.image(yt.thumbnail_url)
 
         yt_streams = yt.streams
 
-        # pre-select the resolution in yt_streams: 144p, 240p, 360p, 480p, 720p, 1080p, 1440p, 2160p
-        avialable_stream = defaultdict(str)
-        for res in resolutions:
-            yt_stream = yt_streams.filter(res=res, type="video").first()
-            if yt_stream is not None:
-                print(f"Found {res} stream")
-                print(yt_stream.is_progressive)
-                avialable_stream[res] = yt_stream
+        # Collect all available resolutions
+        available_resolutions = set()
+        for stream in yt_streams.filter(type="video"):
+            available_resolutions.add(stream.resolution)
 
-        # show a list of available streams
-        st.write("Available streams:")
-        for res in avialable_stream:
-            st.write(f"{res}: {avialable_stream[res]}")
+        resolutions = ["144p", "240p", "360p", "480p", "720p", "1080p", "1440p", "2160p"]
+        # extract the available streams with the selected resolutions
+        resolutions = [res for res in resolutions if res in available_resolutions]
+
 
         # dropdown to select the resolution
-        resolution = st.selectbox("Select resolution", resolutions)
+        resolution = st.selectbox("Select resolution", resolutions, index=resolutions.index("480p") if "480p" in resolutions else 0)
 
         # Add a download button for the video in MP3 format
-        if st.button("Download"):
+        if st.button("Download") and resolution is not None:
             try:
                 with st.spinner("Downloading..."):
                     # get the video stream with certain resolution
-                    video_stream = yt.streams.filter(res="1080p").first()
-                    # if the video stream is not available, get the highest resolution video stream
-                    if video_stream is None:
-                        video_stream = yt.streams.get_highest_resolution()
+                    video_stream = yt.streams.filter(res=resolution).first()
                     # download the video
-                    video_stream.download(filename="Video.mp4")
+                    video_stream.download(filename=f"{output_folder}/video.mp4")
 
                     # get the audio stream
                     audio_stream = yt.streams.get_audio_only()
                     # download the audio
-                    audio_stream.download(filename="Audio.mp3")
+                    audio_stream.download(filename=f"{output_folder}/audio.mp3")
 
                     # Merge the video and audio files into a single video file
-                    video = VideoFileClip("Video.mp4")
-                    audio = AudioFileClip("Audio.mp3")
+                    video = VideoFileClip(f"{output_folder}/video.mp4")
+                    audio = AudioFileClip(f"{output_folder}/audio.mp3")
                     # set the audio of the video as the audio file
                     merge_video = video.set_audio(audio)
                     # specify the output video format codec as H.264 and audio codec as AAC
-
+                    
                     merge_video.write_videofile(
                         f"{output_folder}/{yt.title}.mp4",
                         codec="libx264",
@@ -84,10 +86,10 @@ if "youtube.com" in mp4_url or "youtu.be" in mp4_url:
                         fps=30,
                     )
 
-                    # Display a success message to the user
                 st.success("Download complete!")
             except:
                 st.error("Download failed.")
+
             with open(f"{output_folder}/{yt.title}.mp4", "rb") as f:
                 byte_content = f.read()
                 st.download_button(
@@ -99,3 +101,5 @@ if "youtube.com" in mp4_url or "youtu.be" in mp4_url:
     except:
         # Display an error message if the video cannot be downloaded or converted
         st.error("Download failed!")
+else:
+    st.warning("Please enter a valid YouTube URL.")
